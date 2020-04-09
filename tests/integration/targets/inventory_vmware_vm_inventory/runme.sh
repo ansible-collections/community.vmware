a73839120fdd27a4142d145080b66e268a546cd1
Nasser Alansari <alansari.n@gmail.com>
2020-04-09 15:50:20 +0400

    Refactor tests for Zuul

--- tests/integration/targets/inventory_vmware_vm_inventory/runme.sh
+++ tests/integration/targets/inventory_vmware_vm_inventory/runme.sh
@@ -1,19 +1,13 @@
 #!/usr/bin/env bash
+set -eux
 
-[[ -n "$DEBUG" || -n "$ANSIBLE_DEBUG" ]] && set -x
-
-set -euxo pipefail
-
-# Required to differentiate between Python 2 and 3 environ
+# Envs
 export ANSIBLE_PYTHON_INTERPRETER=${ANSIBLE_TEST_PYTHON_INTERPRETER:-$(which python)}
-export PYTHON=${ANSIBLE_PYTHON_INTERPRETER}
 export ANSIBLE_INVENTORY_ENABLED="community.vmware.vmware_vm_inventory,host_list,ini"
-
-# Cache setting
 export ANSIBLE_CACHE_PLUGIN_CONNECTION="${PWD}/inventory_cache"
 export ANSIBLE_CACHE_PLUGIN="community.general.jsonfile"
 
-INVENTORY_DIR="${PWD}/_test/hosts"
+export INVENTORY_DIR="${PWD}/_test/hosts"
 mkdir -p "${INVENTORY_DIR}" 2>/dev/null
 touch "${INVENTORY_DIR}/empty.yml"
 
@@ -30,37 +24,32 @@ cleanup() {
         rm -rf "${INVENTORY_DIR}"
     fi
 
+    unset ANSIBLE_INVENTORY_ENABLED
+    unset ANSIBLE_CACHE_PLUGIN ANSIBLE_CACHE_PLUGIN_CONNECTION 
+    unset INVENTORY_DIR
+
     echo "Done"
     exit $ec
 }
 trap cleanup INT TERM EXIT
 
-set_inventory(){
-    export ANSIBLE_INVENTORY="${1}"
-    echo "INVENTORY '${1}' is active" 
-}
-
-
 # Prepare tests
-ansible-playbook -i 'localhost,' playbook/prepare_vmware.yml "$@"
-
-# Set inventory path
-set_inventory "${INVENTORY_DIR}"
+ansible-playbook        -i 'localhost,' playbook/prepare_vmware.yml "$@"
 
 # Test Cache
-ansible-playbook -i 'localhost,' playbook/build_inventory_with_cache.yml "$@"
-ansible-inventory --list 1>/dev/null
-ansible-playbook playbook/test_inventory_cache.yml -e inventory_cache="${ANSIBLE_CACHE_PLUGIN_CONNECTION}" "$@"
+ansible-playbook        -i 'localhost,' playbook/build_inventory_with_cache.yml "$@"
+ansible-inventory       -i "${INVENTORY_DIR}" --list 1>/dev/null
+ansible-playbook        -i "${INVENTORY_DIR}" playbook/test_inventory_cache.yml "$@"
 
 # Test YAML and TOME
-ansible-playbook -i 'localhost,' playbook/build_inventory_without_cache.yml "$@"
-ansible-inventory --list --yaml 1>/dev/null
-if ${PYTHON} -m pip list 2>/dev/null | grep toml >/dev/null 2>&1; then
-    ansible-inventory --list --toml 1>/dev/null
+ansible-playbook        -i 'localhost,' playbook/build_inventory_without_cache.yml "$@"
+ansible-inventory       -i "${INVENTORY_DIR}" --list --yaml 1>/dev/null
+if ${ANSIBLE_PYTHON_INTERPRETER} -m pip list 2>/dev/null | grep toml >/dev/null 2>&1; then
+    ansible-inventory   -i "${INVENTORY_DIR}" --list --toml 1>/dev/null
 fi
 
 # # Test playbook with given inventory
-ansible-playbook playbook/test_vmware_vm_inventory.yml "$@"
+ansible-playbook        -i "${INVENTORY_DIR}" playbook/test_vmware_vm_inventory.yml "$@"
 
 # Test options
-ansible-playbook playbook/test_options.yml "$@"
+exec ansible-playbook   -i "${INVENTORY_DIR}" playbook/test_options.yml "$@"
