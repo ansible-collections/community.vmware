04ff490e13675db143bd424844b6d80be0d528a2
Abhijeet Kasurde <akasurde@redhat.com>
2020-07-14 14:37:27 +0530

    Sanity: Doc fixes for vmware_vcenter_settings
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

--- plugins/modules/vmware_vcenter_settings.py
+++ plugins/modules/vmware_vcenter_settings.py
@@ -9,7 +9,7 @@ from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
 
-DOCUMENTATION = '''
+DOCUMENTATION = r'''
 ---
 module: vmware_vcenter_settings
 short_description: Configures general settings on a vCenter server
@@ -27,12 +27,27 @@ options:
     database:
         description:
             - The database settings for vCenter server.
-            - 'Valid attributes are:'
-            - '- C(max_connections) (int): Maximum connections. (default: 50)'
-            - '- C(task_cleanup) (bool): Task cleanup. (default: true)'
-            - '- C(task_retention) (int): Task retention (days). (default: 30)'
-            - '- C(event_cleanup) (bool): Event cleanup. (default: true)'
-            - '- C(event_retention) (int): Event retention (days). (default: 30)'
+        suboptions:
+            max_connections:
+                type: int
+                description: Maximum connections.
+                default: 50
+            task_cleanup:
+                type: bool
+                description: Task cleanup.
+                default: true
+            task_retention:
+                type: int
+                description: Task retention in days.
+                default: 30
+            event_cleanup:
+                type: bool
+                description: Event cleanup.
+                default: true
+            event_retention:
+                type: int
+                description: Event retention in days.
+                default: 30
         type: dict
         default: {
             max_connections: 50,
@@ -44,20 +59,41 @@ options:
     runtime_settings:
         description:
             - The unique runtime settings for vCenter server.
-            - 'Valid attributes are:'
-            - '- C(unique_id) (int): vCenter server unique ID.'
-            - '- C(managed_address) (str): vCenter server managed address.'
-            - '- C(vcenter_server_name) (str): vCenter server name. (default: FQDN)'
+        suboptions:
+            unique_id:
+                type: int
+                description: vCenter server unique ID.
+            managed_address:
+                type: str
+                description: vCenter server managed address.
+            vcenter_server_name:
+                type: str
+                description: vCenter server name. Default is FQDN.
         type: dict
     user_directory:
         description:
             - The user directory settings for the vCenter server installation.
-            - 'Valid attributes are:'
-            - '- C(timeout) (int): User directory timeout. (default: 60)'
-            - '- C(query_limit) (bool): Query limit. (default: true)'
-            - '- C(query_limit_size) (int): Query limit size. (default: 5000)'
-            - '- C(validation) (bool): Mail Validation. (default: true)'
-            - '- C(validation_period) (int): Validation period. (default: 1440)'
+        suboptions:
+            timeout:
+                type: int
+                description: User directory timeout.
+                default: 60
+            query_limit:
+                type: bool
+                description: Query limit.
+                default: true
+            query_limit_size:
+                type: int
+                description: Query limit size.
+                default: 5000
+            validation:
+                type: bool
+                description: Mail Validation.
+                default: true
+            validation_period:
+                type: int
+                description: Validation period.
+                default: 1440
         type: dict
         default: {
             timeout: 60,
@@ -69,9 +105,13 @@ options:
     mail:
         description:
             - The settings vCenter server uses to send email alerts.
-            - 'Valid attributes are:'
-            - '- C(server) (str): Mail server'
-            - '- C(sender) (str): Mail sender address'
+        suboptions:
+            server:
+                type: str
+                description: Mail server.
+            sender:
+                type: str
+                description: Mail sender address.
         type: dict
         default: {
             server: '',
@@ -80,23 +120,71 @@ options:
     snmp_receivers:
         description:
             - SNMP trap destinations for vCenter server alerts.
-            - 'Valid attributes are:'
-            - '- C(snmp_receiver_1_url) (str): Primary Receiver ULR. (default: "localhost")'
-            - '- C(snmp_receiver_1_enabled) (bool): Enable receiver. (default: True)'
-            - '- C(snmp_receiver_1_port) (int): Receiver port. (default: 162)'
-            - '- C(snmp_receiver_1_community) (str): Community string. (default: "public")'
-            - '- C(snmp_receiver_2_url) (str): Receiver 2 ULR. (default: "")'
-            - '- C(snmp_receiver_2_enabled) (bool): Enable receiver. (default: False)'
-            - '- C(snmp_receiver_2_port) (int): Receiver port. (default: 162)'
-            - '- C(snmp_receiver_2_community) (str): Community string. (default: "")'
-            - '- C(snmp_receiver_3_url) (str): Receiver 3 ULR. (default: "")'
-            - '- C(snmp_receiver_3_enabled) (bool): Enable receiver. (default: False)'
-            - '- C(snmp_receiver_3_port) (int): Receiver port. (default: 162)'
-            - '- C(snmp_receiver_3_community) (str): Community string. (default: "")'
-            - '- C(snmp_receiver_4_url) (str): Receiver 4 ULR. (default: "")'
-            - '- C(snmp_receiver_4_enabled) (bool): Enable receiver. (default: False)'
-            - '- C(snmp_receiver_4_port) (int): Receiver port. (default: 162)'
-            - '- C(snmp_receiver_4_community) (str): Community string. (default: "")'
+        suboptions:
+            snmp_receiver_1_url:
+                type: str
+                description: Primary Receiver ULR.
+                default: "localhost"
+            snmp_receiver_1_enabled:
+                type: bool
+                description: Enable receiver.
+                default: True
+            snmp_receiver_1_port:
+                type: int
+                description: Receiver port.
+                default: 162
+            snmp_receiver_1_community:
+                type: str
+                description: Community string.
+                default: "public"
+            snmp_receiver_2_url:
+                type: str
+                description: Receiver 2 ULR.
+                default: ""
+            snmp_receiver_2_enabled:
+                type: bool
+                description: Enable receiver.
+                default: False
+            snmp_receiver_2_port:
+                type: int
+                description: Receiver port.
+                default: 162
+            snmp_receiver_2_community:
+                type: str
+                description: Community string.
+                default: ""
+            snmp_receiver_3_url:
+                type: str
+                description: Receiver 3 ULR.
+                default: ""
+            snmp_receiver_3_enabled:
+                type: bool
+                description: Enable receiver.
+                default: False
+            snmp_receiver_3_port:
+                type: int
+                description: Receiver port.
+                default: 162
+            snmp_receiver_3_community:
+                type: str
+                description: Community string.
+                default: ""
+            snmp_receiver_4_url:
+                type: str
+                description: Receiver 4 ULR.
+                default: ""
+            snmp_receiver_4_enabled:
+                type: bool
+                description: Enable receiver.
+                default: False
+            snmp_receiver_4_port:
+                type: int
+                description: Receiver port.
+                default: 162
+            snmp_receiver_4_community:
+                type: str
+                description: Community string.
+                default: ""
         type: dict
         default: {
             snmp_receiver_1_url: 'localhost',
@@ -119,9 +207,15 @@ options:
     timeout_settings:
         description:
             - The vCenter server connection timeout for normal and long operations.
-            - 'Valid attributes are:'
-            - '- C(normal_operations) (int) (default: 30)'
-            - '- C(long_operations) (int) (default: 120)'
+        suboptions:
+            normal_operations:
+                type: int
+                description: Normal operation timeout.
+                default: 30
+            long_operations:
+                type: int
+                description: Long operation timeout.
+                default: 120
         type: dict
         default: {
             normal_operations: 30,
