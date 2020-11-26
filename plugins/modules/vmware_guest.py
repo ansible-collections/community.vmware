4be445d3af8eacea0067b0a82f12514e3aafb342
Abhijeet Kasurde <akasurde@redhat.com>
2020-11-26 16:42:13 +0530

    Shorten URL for vApp Properties (#520)
    Shorten URL for vApp Properties
    
    Reviewed-by: https://github.com/apps/ansible-zuul

--- plugins/modules/vmware_guest.py
+++ plugins/modules/vmware_guest.py
@@ -639,9 +639,7 @@ options:
   vapp_properties:
     description:
     - A list of vApp properties.
-    - 'For full list of attributes and types refer to:'
-    - 'U(https://vdc-download.vmware.com/vmwb-repository/dcr-public/6b586ed2-655c-49d9-9029-bc416323cb22/
-      fa0b429a-a695-4c11-b7d2-2cbc284049dc/doc/vim.vApp.PropertyInfo.html)'
+    - 'For full list of attributes and types refer to: U(https://code.vmware.com/apis/704/vsphere/vim.vApp.PropertyInfo.html)'
     type: list
     elements: dict
     suboptions:
