ef20754d8ba9baf182f0a8b439fa39f8c6909afa
Abhijeet Kasurde <akasurde@redhat.com>
2020-12-27 23:19:39 +0530

    vmware_cluster: Consider datacenter while create cluster (#586)
    vmware_cluster: Consider datacenter while create cluster
    
    Reviewed-by: https://github.com/apps/ansible-zuul

--- plugins/modules/vmware_cluster_ha.py
+++ plugins/modules/vmware_cluster_ha.py
@@ -273,7 +273,7 @@ class VMwareCluster(PyVmomi):
         if self.datacenter is None:
             self.module.fail_json(msg="Datacenter %s does not exist." % self.datacenter_name)
 
-        self.cluster = self.find_cluster_by_name(cluster_name=self.cluster_name)
+        self.cluster = self.find_cluster_by_name(cluster_name=self.cluster_name, datacenter_name=self.datacenter)
         if self.cluster is None:
             self.module.fail_json(msg="Cluster %s does not exist." % self.cluster_name)
 
