54811a9e9321cf05978577b5008bc47f8020cb89
Gonéri Le Bouder <goneri@lebouder.net>
2021-04-15 10:24:17 -0400

    DNM

--- plugins/modules/vmware_guest.py
+++ plugins/modules/vmware_guest.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# This module is also sponsored by E.T.A.I. (www.etai.fr)
+# This module was also sponsored by E.T.A.I. (www.etai.fr)
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
