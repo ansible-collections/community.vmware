359bf3d32e0d61dabd54443d077fa3344a2df28d
phospi <phoffmann@spirit21.com>
2021-10-26 14:07:39 +0200

    Update plugins/modules/vmware_datastore_info.py
    Co-authored-by: Abhijeet Kasurde <akasurde@redhat.com>

--- plugins/modules/vmware_datastore_info.py
+++ plugins/modules/vmware_datastore_info.py
@@ -73,6 +73,7 @@ options:
      - Tags related to Datastore are shown if set to C(True).
      default: false
      type: bool
+     version_added: '1.16.0'
    properties:
      description:
      - Specify the properties to retrieve.
