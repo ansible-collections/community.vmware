dcdd4c4b7405254fb684ee0adc3470258104908f
Tomas Odehnal <62995026+tomasodehnal@users.noreply.github.com>
2021-12-06 09:53:10 +0100

    vmware_guest: fix the Windows Administrator password being set to string 'None' (#1025)
    vmware_guest: fix the Windows Administrator password being set to string 'None'
    
    SUMMARY
    
    When using Windows VM customization and not specifying a password, the Administrator password is reset to string 'None'. This PR is making the parameter mandatory. This PR is fixing that - when password is not defined, it is cleared (https://vdc-download.vmware.com/vmwb-repository/dcr-public/b50dcbbf-051d-4204-a3e7-e1b618c1e384/538cf2ec-b34f-4bae-a332-3820ef9e7773/vim.vm.customization.GuiUnattended.html).
    Fixes: #1017
    
    ISSUE TYPE
    
    
    Bugfix Pull Request
    
    COMPONENT NAME
    
    vmware_guest
    ADDITIONAL INFORMATION
    
    Reviewed-by: Mario Lenz <m@riolenz.de>
    Reviewed-by: Tomas Odehnal <None>
    Reviewed-by: None <None>

--- plugins/modules/vmware_guest.py
+++ plugins/modules/vmware_guest.py
@@ -681,6 +681,7 @@ options:
             type: str
             description:
             - Local administrator password.
+            - If not defined, the password will be set to blank (that is, no password).
             - Specific to Windows customization.
         productid:
             type: str
@@ -2215,7 +2216,9 @@ class PyVmomiHelper(PyVmomi):
 
             ident.identification = vim.vm.customization.Identification()
 
-            if self.params['customization'].get('password', '') != '':
+            if self.params['customization']['password'] is None or self.params['customization']['password'] == '':
+                ident.guiUnattended.password = None
+            else:
                 ident.guiUnattended.password = vim.vm.customization.Password()
                 ident.guiUnattended.password.value = str(self.params['customization']['password'])
                 ident.guiUnattended.password.plainText = True
