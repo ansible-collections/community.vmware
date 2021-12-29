70cf74ba6211d51674c87b1666f1013efd0f4b80
Mario Lenz <m@riolenz.de>
2021-12-29 15:52:33 +0100

    vmware_guest_network: Fix uncaught exception

--- plugins/modules/vmware_guest_network.py
+++ plugins/modules/vmware_guest_network.py
@@ -330,8 +330,9 @@ except ImportError:
     pass
 
 import copy
+from ansible.module_utils._text import to_native
 from ansible.module_utils.basic import AnsibleModule
-from ansible_collections.community.vmware.plugins.module_utils.vmware import PyVmomi, vmware_argument_spec, wait_for_task
+from ansible_collections.community.vmware.plugins.module_utils.vmware import PyVmomi, TaskError, vmware_argument_spec, wait_for_task
 
 
 class PyVmomiHelper(PyVmomi):
@@ -792,6 +793,8 @@ class PyVmomiHelper(PyVmomi):
                 wait_for_task(task)
             except (vim.fault.InvalidDeviceSpec, vim.fault.RestrictedVersion) as e:
                 self.module.fail_json(msg='failed to reconfigure guest', detail=e.msg)
+            except TaskError as task_e:
+                self.module.fail_json(msg=to_native(task_e))
 
             if task.info.state == 'error':
                 self.module.fail_json(msg='failed to reconfigure guest', detail=task.info.error.msg)
