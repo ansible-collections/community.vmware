eb6382610062de21f5c8e5a25bf68303ddf67f92
Abhijeet Kasurde <akasurde@redhat.com>
2022-01-06 17:52:05 +0530

    Apply suggestions from code review
    Co-authored-by: Felix Fontein <felix@fontein.de>

--- plugins/module_utils/version.py
+++ plugins/module_utils/version.py
@@ -10,9 +10,9 @@ from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
 
-# Once we drop support for Ansible 2.9 and ansible-base 2.10, we can
+# Once we drop support for Ansible 2.9, ansible-base 2.10, and ansible-core 2.11, we can
 # remove the _version.py file, and replace the following import by
 #
 #     from ansible.module_utils.compat.version import LooseVersion
 
-from ._version import LooseVersion
+from ._version import LooseVersion  # noqa: F401
