25317bc257e47244f680e6a0ff4671d87f5008e5
Abhijeet Kasurde <akasurde@redhat.com>
2022-01-06 14:00:56 +0530

    Incorporate new changes in version module
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

--- plugins/module_utils/version.py
+++ plugins/module_utils/version.py
@@ -6,14 +6,13 @@
 """Provide version object to compare version numbers."""
 
 from __future__ import absolute_import, division, print_function
+
 __metaclass__ = type
 
-from ansible.module_utils.six import raise_from
 
-try:
-    from ansible.module_utils.compat.version import LooseVersion  # noqa: F401
-except ImportError:
-    try:
-        from distutils.version import LooseVersion  # noqa: F401
-    except ImportError as exc:
-        raise_from(ImportError('To use this plugin or module with ansible-core < 2.11, you need to use Python < 3.12 with distutils.version present'), exc)
+# Once we drop support for Ansible 2.9 and ansible-base 2.10, we can
+# remove the _version.py file, and replace the following import by
+#
+#     from ansible.module_utils.compat.version import LooseVersion
+
+from ._version import LooseVersion
