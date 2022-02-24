fe489b2ebf383a3b26d88efd5bbd2c49a3b901c0
Mario Lenz <m@riolenz.de>
2022-02-24 13:33:58 +0100

    Remove FQCN from jsonfile plugin in tests

--- tests/integration/targets/inventory_vmware_vm_inventory/runme.sh
+++ tests/integration/targets/inventory_vmware_vm_inventory/runme.sh
@@ -5,7 +5,7 @@ set -eux
 export ANSIBLE_PYTHON_INTERPRETER=${ANSIBLE_TEST_PYTHON_INTERPRETER:-$(command -v python)}
 export ANSIBLE_INVENTORY_ENABLED="community.vmware.vmware_vm_inventory,host_list,ini"
 export ANSIBLE_CACHE_PLUGIN_CONNECTION="${PWD}/inventory_cache"
-export ANSIBLE_CACHE_PLUGIN="community.general.jsonfile"
+export ANSIBLE_CACHE_PLUGIN="jsonfile"
 
 export INVENTORY_DIR="${PWD}/_test/hosts"
 mkdir -p "${INVENTORY_DIR}" 2>/dev/null
@@ -36,12 +36,13 @@ trap cleanup INT TERM EXIT
 ansible-playbook playbook/prepare_vmware.yml "$@"
 
 # Test Cache
-# Cache requires community.general.jsonfile
+# Cache requires jsonfile
 ansible-playbook playbook/build_inventory_with_cache.yml "$@"
 ansible-inventory -i "${INVENTORY_DIR}" --list 1>/dev/null
 ansible-playbook -i "${INVENTORY_DIR}" playbook/test_inventory_cache.yml "$@"
 
 # Test YAML and TOML
+ansible-playbook playbook/install_dependencies.yml "$@"
 ansible-playbook playbook/build_inventory_without_cache.yml "$@"
 ansible-inventory -i "${INVENTORY_DIR}" --list --yaml 1>/dev/null
 if ${ANSIBLE_PYTHON_INTERPRETER} -m pip list 2>/dev/null | grep toml >/dev/null 2>&1; then
