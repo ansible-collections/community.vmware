4452154eae35dbaed6b47f2d51bf58d3db13d1cd
Nina2244 <26925042+Nina2244@users.noreply.github.com>
2022-02-25 15:51:46 +0100

    Bugfix: Only Storage vmotion
    The Example Below didn't work:
    - name: Perform storage vMotion of virtual machine
      community.vmware.vmware_vmotion:
        hostname: '{{ vcenter_hostname }}'
        username: '{{ vcenter_username }}'
        password: '{{ vcenter_password }}'
        vm_name: 'vm_name_as_per_vcenter'
        destination_datastore: 'destination_datastore_as_per_vcenter'
      delegate_to: localhost
    
    With removing this from the code it works:
    # Fail if resourcePool object is not found
    if self.resourcepool_object is None:
      self.module.fail_json(msg="Unable to find destination resource pool object which is required")

--- plugins/modules/vmware_vmotion.py
+++ plugins/modules/vmware_vmotion.py
@@ -204,9 +204,6 @@ class VmotionManager(PyVmomi):
                                                                   resource_pool_name=dest_resourcepool)
         elif not dest_resourcepool and dest_host_name:
             self.resourcepool_object = self.host_object.parent.resourcePool
-        # Fail if resourcePool object is not found
-        if self.resourcepool_object is None:
-            self.module.fail_json(msg="Unable to find destination resource pool object which is required")
 
         # Check if datastore is required, this check is required if destination
         # and source host system does not share same datastore.
