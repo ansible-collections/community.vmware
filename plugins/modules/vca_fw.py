1953a5fad040afe3f9528ef4171e6af8ea88f049
Gonéri Le Bouder <goneri@lebouder.net>
2022-04-08 14:34:46 -0400

    DNM - CI TEST
    Depends-On: https://github.com/ansible/ansible-zuul-jobs/pull/1478

--- plugins/modules/vca_fw.py
+++ plugins/modules/vca_fw.py
@@ -3,9 +3,6 @@
 # Copyright: (c) 2015, VMware, Inc. All Rights Reserved.
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-from __future__ import absolute_import, division, print_function
-__metaclass__ = type
-
 
 DOCUMENTATION = r'''
 ---
