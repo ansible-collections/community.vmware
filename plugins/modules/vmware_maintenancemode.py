b6f2b8f368520cbe82e5dc6d5683c90798919132
Marius Rieder <marius.rieder@scs.ch>
2022-05-06 12:01:16 +0200

    vmware_maintenancemode: Add support for check_mode

--- plugins/modules/vmware_maintenancemode.py
+++ plugins/modules/vmware_maintenancemode.py
@@ -132,11 +132,14 @@ class VmwareMaintenanceMgr(PyVmomi):
             spec.vsanMode.objectAction = self.vsan
 
         try:
-            task = self.host.EnterMaintenanceMode_Task(self.module.params['timeout'],
-                                                       self.module.params['evacuate'],
-                                                       spec)
+            if not self.module.check_mode:
+                task = self.host.EnterMaintenanceMode_Task(self.module.params['timeout'],
+                                                           self.module.params['evacuate'],
+                                                           spec)
 
-            success, result = wait_for_task(task)
+                success, result = wait_for_task(task)
+            else:
+                success = True
 
             self.module.exit_json(changed=success,
                                   hostsystem=str(self.host),
@@ -156,9 +159,12 @@ class VmwareMaintenanceMgr(PyVmomi):
                                   msg='Host %s not in maintenance mode' % self.esxi_hostname)
 
         try:
-            task = self.host.ExitMaintenanceMode_Task(self.module.params['timeout'])
+            if not self.module.check_mode:
+                task = self.host.ExitMaintenanceMode_Task(self.module.params['timeout'])
 
-            success, result = wait_for_task(task)
+                success, result = wait_for_task(task)
+            else:
+                success = True
 
             self.module.exit_json(changed=success,
                                   hostsystem=str(self.host),
@@ -184,7 +190,8 @@ def main():
                      )
                 )
 
-    module = AnsibleModule(argument_spec=spec)
+    module = AnsibleModule(argument_spec=spec,
+                           supports_check_mode=True)
 
     host_maintenance_mgr = VmwareMaintenanceMgr(module=module)
 
