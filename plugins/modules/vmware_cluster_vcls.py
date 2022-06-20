792e34135186abb01dd5af7b7e33d51b3c2a795b
Nina2244 <26925042+Nina2244@users.noreply.github.com>
2022-06-20 10:44:11 +0200

    Linter

--- plugins/modules/vmware_cluster_vcls.py
+++ plugins/modules/vmware_cluster_vcls.py
@@ -86,7 +86,6 @@ from ansible_collections.community.vmware.plugins.module_utils.vmware import (
     find_datastore_by_name,
     vmware_argument_spec,
     wait_for_task,
-    option_diff,
 )
 from ansible.module_utils._text import to_native
 
