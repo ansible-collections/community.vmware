4738b109e9dd2616e475c54b580e0121c39ada81
Nina2244 <26925042+Nina2244@users.noreply.github.com>
2022-06-20 16:00:15 +0200

    Try it ;)

--- plugins/modules/vmware_dvswitch.py
+++ plugins/modules/vmware_dvswitch.py
@@ -227,13 +227,13 @@ options:
                 default: False
         type: dict
         default: {
-            collector_ip='',
-            collector_port=0,
-            observation_domain_id=0,
-            active_flow_timeout=60,
-            idle_flow_timeout=15,
-            sampling_rate=4096,
-            internal_flows_only=False,
+            'collector_ip'='',
+            'collector_port'=0,
+            'observation_domain_id'=0,
+            'active_flow_timeout'=60,
+            'idle_flow_timeou't=15,
+            'sampling_rate'=4096,
+            'internal_flows_only'=False,
         }
 extends_documentation_fragment:
 - community.vmware.vmware.documentation
