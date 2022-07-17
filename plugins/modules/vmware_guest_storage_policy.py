05b1565378b4424182d8f38caf61484fa0db3c5c
Mario Lenz <m@riolenz.de>
2022-07-17 16:45:06 +0200

    Add SPDX-License-Identifier to modules

--- plugins/modules/vmware_guest_storage_policy.py
+++ plugins/modules/vmware_guest_storage_policy.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-#
+
 # Copyright: (c) 2021, Tyler Gates <tgates81@gmail.com>
 #
 # Special thanks to:
@@ -15,7 +15,8 @@
 #                                             modeled after
 #                                             `vmware_guest_disk.py'.
 #
-# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+# GNU General Public License v3.0+ (see LICENSES/GPL-3.0-or-later.txt or https://www.gnu.org/licenses/gpl-3.0.txt)
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
