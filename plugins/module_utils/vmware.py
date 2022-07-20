19b8ffed6d6220d866dd4f87c3180d3a5fdf873a
Mario Lenz <m@riolenz.de>
2022-07-20 23:15:12 +0200

    Use SPDX-License-Identifier, mention all licenses in galaxy.yml, add missing license file (#1398)
    Use SPDX-License-Identifier, mention all licenses in galaxy.yml, add missing license file
    
    SUMMARY
    Implements ansible-community/community-topics#112 (comment).
    Also adds a missing license file for the BSD-2-Clause licensed vmware_spbm and vmware_rest_client module utils.
    ISSUE TYPE
    
    Docs Pull Request
    Feature Pull Request
    
    COMPONENT NAME
    collection
    ADDITIONAL INFORMATION
    I've also tried to unify the format. For example, sometimes there was an empty comment before the copyright (# ) and sometimes there was an empty line without a comment.
    cc @felixfontein
    
    Reviewed-by: Felix Fontein <felix@fontein.de>

--- plugins/module_utils/vmware.py
+++ plugins/module_utils/vmware.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
+
 # Copyright: (c) 2015, Joseph Callen <jcallen () csc.com>
 # Copyright: (c) 2018, Ansible Project
 # Copyright: (c) 2018, James E. King III (@jeking3) <jking@apache.org>
-# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+# GNU General Public License v3.0+ (see LICENSES/GPL-3.0-or-later.txt or https://www.gnu.org/licenses/gpl-3.0.txt)
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
