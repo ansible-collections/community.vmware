7e6ae667be889d0b4d272aa4cdc2a9c8e99b0193
Jared Priddy <jdptechnc@gmail.com>
2022-12-23 15:05:50 -0500

    return list of vgpus, update documentation

--- plugins/modules/vmware_guest_vgpu_info.py
+++ plugins/modules/vmware_guest_vgpu_info.py
@@ -12,15 +12,13 @@ __metaclass__ = type
 
 DOCUMENTATION = r"""
 ---
-module: vmware_guest_vgpu
-short_description: Modify vGPU video card profile of the specified virtual machine in the given vCenter infrastructure
+module: vmware_guest_vgpu_info
+short_description: Gather information about vGPU profiles of the specified virtual machine in the given vCenter infrastructure
 description:
-    - This module is used to reconfigure vGPU card profile of the given virtual machine.
+    - This module is used to gather metadata about vGPU card profiles of the given virtual machine.
     - All parameters and VMware object names are case sensitive.
-    - VM must be power off M(community.vmware.vmware_guest_powerstate) module can perform that task.
 author:
-    - Mohamed Alibi (@Medalibi)
-    - Unknown (@matancarmeli7)
+    - Jared Priddy (@jdptechnc)
 options:
    name:
      description:
@@ -66,11 +64,10 @@ options:
      type: bool
 extends_documentation_fragment:
 - community.vmware.vmware.documentation
-version_added: '2.5.0'
 """
 
 EXAMPLES = r"""
-- name: Gather information about vGPU profile of a VM
+- name: Gather information about vGPU profiles of a VM
   community.vmware.vmware_guest_vgpu_info:
     hostname: "{{ vcenter_hostname }}"
     username: "{{ vcenter_username }}"
@@ -85,18 +82,20 @@ EXAMPLES = r"""
 
 RETURN = r"""
 vgpu_info:
-    description: metadata about the virtual machine's vGPU profile
+    description: metadata about the virtual machine's vGPU profiles
     returned: always
-    type: dict
+    type: list
     sample: {
-        "vgpu": {
-            "Controller_Key": 100,
-            "Key": 13000,
-            "Label": "PCI device 0",
-            "Summary": "NVIDIA GRID vGPU grid_m10-8q",
-            "Unit_Number": 18,
-            "Vgpu": "grid_m10-8q"
-        }
+        "vgpu": [
+            {
+                "Controller_Key": 100,
+                "Key": 13000,
+                "Label": "PCI device 0",
+                "Summary": "NVIDIA GRID vGPU grid_m10-8q",
+                "Unit_Number": 18,
+                "Vgpu": "grid_m10-8q"
+            }
+        ]
     }
 """
 
@@ -122,12 +121,12 @@ class PyVmomiHelper(PyVmomi):
         Gather facts about VM's vGPU profile settings
         Args:
             vm_obj: Managed object of virtual machine
-        Returns: vGPU profile and its facts
+        Returns: list of vGPU profiles with facts
         """
-        vgpu_info = dict()
+        vgpu_info = []
         for vgpu_VirtualDevice_obj in vm_obj.config.hardware.device:
             if hasattr(vgpu_VirtualDevice_obj.backing, "vgpu"):
-                vgpu_info = dict(
+                vgpu = dict(
                     Vgpu=vgpu_VirtualDevice_obj.backing.vgpu,
                     Key=vgpu_VirtualDevice_obj.key,
                     Summary=vgpu_VirtualDevice_obj.deviceInfo.summary,
@@ -135,168 +134,8 @@ class PyVmomiHelper(PyVmomi):
                     Unit_Number=vgpu_VirtualDevice_obj.unitNumber,
                     Controller_Key=vgpu_VirtualDevice_obj.controllerKey,
                 )
-                break
+                vgpu_info.append(vgpu)
         return vgpu_info
-
-#     def _vgpu_absent(self, vm_obj):
-#         """
-#         Remove vGPU profile of virtual machine.
-#         Args:
-#             vm_obj: Managed object of virtual machine
-#         Returns: Operation results and vGPU facts
-#         """
-#         result = {}
-#         vgpu_prfl = self.params["vgpu"]
-#         vgpu_VirtualDevice_obj = self._get_vgpu_VirtualDevice_object(vm_obj, vgpu_prfl)
-#         if vgpu_VirtualDevice_obj is None:
-#             changed = False
-#             failed = False
-#         else:
-#             vgpu_fact = self._gather_vgpu_profile_facts(vm_obj)
-#             changed, failed = self._remove_vgpu_profile_from_vm(
-#                 vm_obj, vgpu_VirtualDevice_obj, vgpu_prfl
-#             )
-#         result = {"changed": changed, "failed": failed, "vgpu": vgpu_fact}
-#         return result
-
-#     def _remove_vgpu_profile_from_vm(self, vm_obj, vgpu_VirtualDevice_obj, vgpu_prfl):
-#         """
-#         Remove vGPU profile of virtual machine
-#         Args:
-#             vm_obj: Managed object of virtual machine
-#             vgpu_VirtualDevice_obj: vGPU profile object holding its facts
-#             vgpu_prfl: vGPU profile name
-#         Returns: Operation results
-#         """
-#         changed = False
-#         failed = False
-#         vm_current_vgpu_profile = self._get_vgpu_profile_in_the_vm(vm_obj)
-#         if vgpu_prfl in vm_current_vgpu_profile:
-#             vdspec = vim.vm.device.VirtualDeviceSpec()
-#             vmConfigSpec = vim.vm.ConfigSpec()
-#             vdspec.operation = "remove"
-#             vdspec.device = vgpu_VirtualDevice_obj
-#             vmConfigSpec.deviceChange.append(vdspec)
-
-#             try:
-#                 task = vm_obj.ReconfigVM_Task(spec=vmConfigSpec)
-#                 wait_for_task(task)
-#                 changed = True
-#                 return changed, failed
-#             except Exception as exc:
-#                 failed = True
-#                 self.module.fail_json(
-#                     msg="Failed to delete vGPU profile"
-#                     " '%s' from vm %s." % (vgpu_prfl, vm_obj.name),
-#                     detail=exc.msg,
-#                 )
-#         return changed, failed
-
-#     def _vgpu_present(self, vm_obj):
-#         """
-#         Add vGPU profile to virtual machine.
-#         Args:
-#             vm_obj: Managed object of virtual machine
-#         Returns: Operation results and vGPU facts
-#         """
-#         result = {}
-#         vgpu_prfl = self.params["vgpu"]
-#         vgpu_profile_name = self._get_vgpu_profiles_name(vm_obj, vgpu_prfl)
-#         if vgpu_profile_name is None:
-#             self.module.fail_json(
-#                 msg="vGPU Profile '%s'" " does not exist." % vgpu_prfl
-#             )
-
-#         changed, failed = self._add_vgpu_profile_to_vm(
-#             vm_obj, vgpu_profile_name, vgpu_prfl
-#         )
-#         vgpu_fact = self._gather_vgpu_profile_facts(vm_obj)
-#         result = {"changed": changed, "failed": failed, "vgpu": vgpu_fact}
-#         return result
-
-#     def _add_vgpu_profile_to_vm(self, vm_obj, vgpu_profile_name, vgpu_prfl):
-#         """
-#         Add vGPU profile of virtual machine
-#         Args:
-#             vm_obj: Managed object of virtual machine
-#             vgpu_profile_name: vGPU profile object name from ESXi server list
-#             vgpu_prfl: vGPU profile name
-#         Returns: Operation results
-#         """
-#         changed = False
-#         failed = False
-#         vm_current_vgpu_profile = self._get_vgpu_profile_in_the_vm(vm_obj)
-#         if self.params["force"] or vgpu_prfl not in vm_current_vgpu_profile:
-#             vgpu_p = vgpu_profile_name.vgpu
-#             backing = vim.VirtualPCIPassthroughVmiopBackingInfo(vgpu=vgpu_p)
-#             summary = "NVIDIA GRID vGPU " + vgpu_prfl
-#             deviceInfo = vim.Description(summary=summary, label="PCI device 0")
-#             hba_object = vim.VirtualPCIPassthrough(
-#                 backing=backing, deviceInfo=deviceInfo
-#             )
-#             new_device_config = vim.VirtualDeviceConfigSpec(device=hba_object)
-#             new_device_config.operation = "add"
-#             vmConfigSpec = vim.vm.ConfigSpec()
-#             vmConfigSpec.deviceChange = [new_device_config]
-#             vmConfigSpec.memoryReservationLockedToMax = True
-
-#             try:
-#                 task = vm_obj.ReconfigVM_Task(spec=vmConfigSpec)
-#                 wait_for_task(task)
-#                 changed = True
-#             except Exception as exc:
-#                 failed = True
-#                 self.module.fail_json(
-#                     msg="Failed to add vGPU Profile"
-#                     " '%s' to vm %s." % (vgpu_prfl, vm_obj.name),
-#                     detail=exc.msg,
-#                 )
-#         else:
-#             return changed, failed
-#         return changed, failed
-
-#     def _get_vgpu_profile_in_the_vm(self, vm_obj):
-#         """
-#         Get vGPU profile object of virtual machine
-#         Args:
-#             vm_obj: Managed object of virtual machine
-#         Returns: vGPU profile name
-#         """
-#         vm_current_vgpu_profile = []
-#         for vgpu_VirtualDevice_obj in vm_obj.config.hardware.device:
-#             if hasattr(vgpu_VirtualDevice_obj.backing, "vgpu"):
-#                 vm_current_vgpu_profile.append(vgpu_VirtualDevice_obj.backing.vgpu)
-#         return vm_current_vgpu_profile
-
-#     def _get_vgpu_VirtualDevice_object(self, vm_obj, vgpu_prfl):
-#         """
-#         Get current vGPU profile object of virtual machine
-#         Args:
-#             vm_obj: Managed object of virtual machine
-#             vgpu_prfl: vGPU profile name
-#         Returns: vGPU profile name of virtual machine
-#         """
-#         for vgpu_VirtualDevice_obj in vm_obj.config.hardware.device:
-#             if hasattr(vgpu_VirtualDevice_obj.backing, "vgpu"):
-#                 if vgpu_VirtualDevice_obj.backing.vgpu == vgpu_prfl:
-#                     return vgpu_VirtualDevice_obj
-#         return None
-
-#     def _get_vgpu_profiles_name(self, vm_obj, vgpu_prfl):
-#         """
-#         Get matched vGPU profile object of ESXi host
-#         Args:
-#             vm_obj: Managed object of virtual machine
-#             vgpu_prfl: vGPU profile name
-#         Returns: vGPU profile object
-#         """
-#         vm_host = vm_obj.runtime.host
-#         vgpu_profiles = vm_host.config.sharedGpuCapabilities
-#         for vgpu_profile_name in vgpu_profiles:
-#             if vgpu_profile_name.vgpu == vgpu_prfl:
-#                 return vgpu_profile_name
-#         return None
-
 
 def main():
     argument_spec = vmware_argument_spec()
@@ -307,23 +146,16 @@ def main():
         moid=dict(type="str"),
         folder=dict(type="str"),
         datacenter=dict(type="str", default="ha-datacenter"),
-#         esxi_hostname=dict(type="str"),
-#         cluster=dict(type="str"),
-#         vgpu=dict(type="str"),
-#         force=dict(type="bool", default=False),
-#         state=dict(type="str", default="present", choices=["absent", "present"]),
     )
 
     module = AnsibleModule(
         argument_spec=argument_spec,
-#         mutually_exclusive=[["cluster", "esxi_hostname"]],
         required_one_of=[["name", "uuid", "moid"]],
         supports_check_mode=True,
     )
 
     pyv = PyVmomiHelper(module)
     vm = pyv.get_vm()
-
 
     if not vm:
         vm_id = (
@@ -337,23 +169,9 @@ def main():
         
     else:
         try:
-            module.exit_json(vgpu_info=pyv.gather_vgpu_profile_facts(vm))
+            module.exit_json(vgpu=pyv.gather_vgpu_profile_facts(vm))
         except Exception as exc:
             module.fail_json(msg="Failed to gather information with exception : %s" % to_text(exc))      
-
-#     if module.params["state"] == "present":
-#         result = pyv._vgpu_present(vm)
-#     elif module.params["state"] == "absent":
-#         result = pyv._vgpu_absent(vm)
-
-#     if "failed" not in result:
-#         result["failed"] = False
-
-#     if result["failed"]:
-#         module.fail_json(**result)
-#     else:
-#         module.exit_json(**result)
-
 
 if __name__ == "__main__":
     main()
