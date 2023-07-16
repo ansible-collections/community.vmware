bc780346d9c079f3a908c4cce0124f0b494e9706
Mario Lenz <m@riolenz.de>
2023-07-16 16:47:12 +0000

    prepare 3.8.0 release

--- tools/antsibull-docs-wrapper.sh
+++ tools/antsibull-docs-wrapper.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-TMPDIR=`mktemp -d`
+TMPDIR=$(mktemp -d)
 export ANSIBLE_COLLECTIONS_PATH="$TMPDIR"
 ansible-galaxy collection install "$1"
 antsibull-docs collection-plugins community.vmware --dest-dir docs/ --use-current --output-format simplified-rst --fqcn-plugin-names
-rm -rf $TMPDIR
+rm -rf "$TMPDIR"
