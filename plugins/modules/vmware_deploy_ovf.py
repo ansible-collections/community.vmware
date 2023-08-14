d03f3dc9a0cf88c0df7e7386835d922cec43da8f
Mario Lenz <m@riolenz.de>
2023-08-14 09:06:08 +0200

    Fix E501: line too long (164 > 160 characters)

--- plugins/modules/vmware_deploy_ovf.py
+++ plugins/modules/vmware_deploy_ovf.py
@@ -276,8 +276,9 @@ class WebHandle(object):
 
     def _parse_url(self, url):
         exp1 = r"(?P<url>(?:(?P<scheme>[a-zA-Z]+:\/\/)?(?P<hostname>(?:[-a-zA-Z0-9@%_\+~#=]{1,256}\.){1,256}(?:[-a-zA-Z0-9@%_\+~#=]{1,256})))"
-        exp2 = r"(?::(?P<port>[[:digit:]]+))?(?P<path>(?:\/[-a-zA-Z0-9!$&'()*+,\\\/:;=@\[\]._~%]*)*)(?P<query>(?:(?:\#|\?)[-a-zA-Z0-9!$&'()*+,\\\/:;=@\[\]._~]*)*))"
-        return re.match(exp1 + exp2, url)
+        exp2 = r"(?::(?P<port>[[:digit:]]+))?(?P<path>(?:\/[-a-zA-Z0-9!$&'()*+,\\\/:;=@\[\]._~%]*)*)"
+        exp3 = r"(?P<query>(?:(?:\#|\?)[-a-zA-Z0-9!$&'()*+,\\\/:;=@\[\]._~]*)*))"
+        return re.match(exp1 + exp2 + exp3, url)
 
     def _get_thumbprint(self, hostname):
         pem = ssl.get_server_certificate((hostname, 443))
