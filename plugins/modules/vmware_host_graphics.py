43cc32d65fd7676c53960ecaf94c8e85416f2540
Mario Lenz <m@riolenz.de>
2023-08-15 18:47:27 +0200

    Update version_added

--- plugins/modules/vmware_host_graphics.py
+++ plugins/modules/vmware_host_graphics.py
@@ -11,7 +11,7 @@ __metaclass__ = type
 DOCUMENTATION = r'''
 ---
 module: vmware_host_graphics
-version_added: '3.9.0'
+version_added: '3.10.0'
 short_description: Manage Host Graphic Settings
 description:
 - This module can be used to manage Host Graphic Settings
