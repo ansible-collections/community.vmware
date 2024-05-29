0b87621794eda029a73a935fb15affe9af1f9394
Lupus <67869327+sudo-lupus@users.noreply.github.com>
2024-05-29 16:08:05 +0200

    linting

--- plugins/modules/vmware_sdrs_vm_overrides_info.py
+++ plugins/modules/vmware_sdrs_vm_overrides_info.py
@@ -6,10 +6,11 @@
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
+
 __metaclass__ = type
 
 
-DOCUMENTATION = r'''
+DOCUMENTATION = r"""
 ---
 module: vmware_sdrs_vm_overrides_info
 short_description: Check for VM overrides in a SDRS datastore cluster
@@ -27,48 +28,51 @@ options:
 extends_documentation_fragment:
 - community.vmware.vmware.documentation
 
-'''
+"""
 
-EXAMPLES = r'''
-- name: Checks a datastore cluster for VMs with Storage DRS enabled
-  community.vmware.vmware_sdrs_vm_overrides_info:
+EXAMPLES = r"""
+- name: Create datastore cluster and enable SDRS
+  swisscom.vmware.vmware_sdrs_vm_overrides_info:
     hostname: '{{ vcenter_hostname }}'
     username: '{{ vcenter_username }}'
     password: '{{ vcenter_password }}'
     datastore_cluster_name: '{{ datastore_cluster_name }}'
   delegate_to: localhost
-'''
+"""
 
-RETURN = r'''
+RETURN = r"""
 result:
     description: information about datastore clusters with VMs with SDRS enabled
     returned: always
     type: str
     sample:  {"changed": false, "result": {"DSC_1": {"vm_overrides": {"VM_1": {"srds_enabled_status": false, "vm_behavior": null}, "VM_2": {"srds_enabled_status": false, "vm_behavior": null}}}}}
-'''
-
-try:
-    from pyVmomi import vim
-except ImportError:
-    pass
+"""
 
 from ansible.module_utils.basic import AnsibleModule
-from ansible_collections.community.vmware.plugins.module_utils.vmware import PyVmomi, vmware_argument_spec
+from ansible_collections.community.vmware.plugins.module_utils.vmware import (
+    PyVmomi,
+    vmware_argument_spec,
+)
 
 
 class VMwareDatastoreClusterManager(PyVmomi):
     def __init__(self, module):
         super(VMwareDatastoreClusterManager, self).__init__(module)
-        self.datastore_cluster_name = self.params.get('datastore_cluster_name')
-        self.datastore_cluster_obj = self.find_datastore_cluster_by_name(self.datastore_cluster_name)
+        self.datastore_cluster_name = self.params.get("datastore_cluster_name")
+        self.datastore_cluster_obj = self.find_datastore_cluster_by_name(
+            self.datastore_cluster_name
+        )
 
     def check_vm_overrides(self):
         """
         Check SDRS status of VMs in datastore cluster
         """
-        results = dict(changed=False, result = {})
+        results = dict(changed=False, result={})
 
-        vm_configs = [vm_spec for vm_spec in self.datastore_cluster_obj.podStorageDrsEntry.storageDrsConfig.vmConfig]
+        vm_configs = [
+            vm_spec
+            for vm_spec in self.datastore_cluster_obj.podStorageDrsEntry.storageDrsConfig.vmConfig
+        ]
         vm_overrides = dict()
 
         for vm in vm_configs:
@@ -78,29 +82,25 @@ class VMwareDatastoreClusterManager(PyVmomi):
                 vm_overrides[vm.vm.name]["vm_behavior"] = vm.behavior
 
         if vm_overrides.keys():
-            results["result"] = dict(
-                vm_overrides = vm_overrides
-                )
+            results["result"] = dict(vm_overrides=vm_overrides)
         else:
             results["result"] = ""
 
         self.module.exit_json(**results)
 
+
 def main():
     argument_spec = vmware_argument_spec()
     argument_spec.update(
         dict(
-            datastore_cluster_name=dict(type='str', required=True),
+            datastore_cluster_name=dict(type="str", required=True),
         )
     )
-    module = AnsibleModule(
-        argument_spec=argument_spec,
-        supports_check_mode=True
-    )
+    module = AnsibleModule(argument_spec=argument_spec, supports_check_mode=True)
 
     datastore_cluster_mgr = VMwareDatastoreClusterManager(module)
     datastore_cluster_mgr.check_vm_overrides()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
