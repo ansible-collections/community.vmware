84de67346d8ea351963ac3e88f267986b4ac31b3
Mario Lenz <m@riolenz.de>
2024-09-29 18:15:42 +0200

    [DNM] stable-3.x: Test CI

--- plugins/modules/vmware_vswitch.py
+++ plugins/modules/vmware_vswitch.py
@@ -256,6 +256,8 @@ from ansible.module_utils.basic import AnsibleModule
 from ansible_collections.community.vmware.plugins.module_utils.vmware import PyVmomi, vmware_argument_spec
 from ansible.module_utils._text import to_native
 
+# Meaningless diff with no code changes to trigger CI
+
 
 class VMwareHostVirtualSwitch(PyVmomi):
     def __init__(self, module):
