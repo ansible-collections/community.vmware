1ebf5516d189974d1cd4e1c65f4b43187ff78b53
Mario Lenz <m@riolenz.de>
2025-03-13 19:23:14 +0100

    Drop unit test for unsupported Python versions

--- tests/unit/modules/cloud/vmware/test_vmware_guest.py
+++ tests/unit/modules/cloud/vmware/test_vmware_guest.py
@@ -6,23 +6,12 @@
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
-import os
-import sys
 import pytest
 import json
 
 pyvmomi = pytest.importorskip('pyVmomi')
 
-if sys.version_info < (2, 7):
-    pytestmark = pytest.mark.skip("vmware_guest Ansible modules require Python >= 2.7")
-
-
 from ansible_collections.community.vmware.plugins.modules import vmware_guest
-
-curr_dir = os.path.dirname(__file__)
-test_data_file = open(os.path.join(curr_dir, 'test_data', 'test_vmware_guest_with_parameters.json'), 'r')
-TEST_CASES = json.loads(test_data_file.read())
-test_data_file.close()
 
 
 @pytest.mark.parametrize('patch_ansible_module', [{}], indirect=['patch_ansible_module'])
@@ -34,19 +23,3 @@ def test_vmware_guest_wo_parameters(capfd):
     results = json.loads(out)
     assert results['failed']
     assert "one of the following is required: name, uuid" in results['msg']
-
-
-@pytest.mark.parametrize('patch_ansible_module, testcase', TEST_CASES, indirect=['patch_ansible_module'])
-@pytest.mark.usefixtures('patch_ansible_module')
-def test_vmware_guest_with_parameters(mocker, capfd, testcase):
-    if testcase.get('test_ssl_context', None):
-        class mocked_ssl:
-            pass
-        mocker.patch('ansible_collections.community.vmware.plugins.module_utils.vmware.ssl', new=mocked_ssl)
-
-    with pytest.raises(SystemExit):
-        vmware_guest.main()
-    out, err = capfd.readouterr()
-    results = json.loads(out)
-    assert str(results['failed']) == testcase['failed']
-    assert testcase['msg'] in results['msg']
