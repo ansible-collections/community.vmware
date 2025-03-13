d741875e0b1e27350f03fbbb4e1448199d9b19d2
Mario Lenz <m@riolenz.de>
2025-03-13 18:54:45 +0100

    Drop unit tests for vmdk_disk_path_split

--- tests/unit/module_utils/test_vmware.py
+++ tests/unit/module_utils/test_vmware.py
@@ -134,28 +134,6 @@ def test_required_params(request, params, msg, fake_ansible_module):
     # TODO: assert msg in fake_ansible_module.fail_json.call_args[1]['msg']
 
 
-def test_vmdk_disk_path_split(monkeypatch, fake_ansible_module):
-    """ Test vmdk_disk_path_split function"""
-    fake_ansible_module.params = test_data[3][0]
-
-    monkeypatch.setattr(vmware_module_utils, 'connect_to_api', fake_connect_to_api)
-    pyv = vmware_module_utils.PyVmomi(fake_ansible_module)
-    v = pyv.vmdk_disk_path_split('[ds1] VM_0001/VM0001_0.vmdk')
-    assert v == ('ds1', 'VM_0001/VM0001_0.vmdk', 'VM0001_0.vmdk', 'VM_0001')
-
-
-def test_vmdk_disk_path_split_negative(monkeypatch, fake_ansible_module):
-    """ Test vmdk_disk_path_split function"""
-    fake_ansible_module.params = test_data[3][0]
-
-    monkeypatch.setattr(vmware_module_utils, 'connect_to_api', fake_connect_to_api)
-    with pytest.raises(FailJsonException):
-        pyv = vmware_module_utils.PyVmomi(fake_ansible_module)
-        pyv.vmdk_disk_path_split('[ds1]')
-    fake_ansible_module.fail_json.assert_called_once()
-    assert 'Bad path' in fake_ansible_module.fail_json.call_args[1]['msg']
-
-
 @pytest.mark.parametrize("test_options, test_current_options, test_truthy_strings_as_bool", [
     ({"data": True}, [], True),
     ({"data": 1}, [], True),
