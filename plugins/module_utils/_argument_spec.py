82f664f5f902f088e869c1962bd8a940f9505fa7
Mario Lenz <m@riolenz.de>
2025-03-29 14:43:46 +0100

    Move vmware_client_argument_spec to dedicated file

--- plugins/module_utils/_argument_spec.py
+++ plugins/module_utils/_argument_spec.py
@@ -1,6 +1,37 @@
 from ansible.module_utils.basic import env_fallback
 
 
+def rest_compatible_argument_spec():
+    return dict(
+        hostname=dict(type='str',
+                      fallback=(env_fallback, ['VMWARE_HOST'])),
+        username=dict(type='str',
+                      fallback=(env_fallback, ['VMWARE_USER']),
+                      aliases=['user', 'admin']),
+        password=dict(type='str',
+                      fallback=(env_fallback, ['VMWARE_PASSWORD']),
+                      aliases=['pass', 'pwd'],
+                      no_log=True),
+        port=dict(type='int',
+                  default=443,
+                  fallback=(env_fallback, ['VMWARE_PORT'])),
+        protocol=dict(type='str',
+                      default='https',
+                      choices=['https', 'http']),
+        validate_certs=dict(type='bool',
+                            fallback=(env_fallback, ['VMWARE_VALIDATE_CERTS']),
+                            default=True),
+        proxy_host=dict(type='str',
+                        required=False,
+                        default=None,
+                        fallback=(env_fallback, ['VMWARE_PROXY_HOST'])),
+        proxy_port=dict(type='int',
+                        required=False,
+                        default=None,
+                        fallback=(env_fallback, ['VMWARE_PROXY_PORT'])),
+    )
+
+
 def base_argument_spec():
     return dict(
         hostname=dict(type='str',
