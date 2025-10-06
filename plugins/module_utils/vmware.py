cf53f971d669fb006e95ce02d3d0030ad89ea23a
Mario Lenz <m@riolenz.de>
2025-10-06 17:36:32 +0200

    stable-5.x: Replace ansible.module_utils.six.moves.urllib

--- plugins/module_utils/vmware.py
+++ plugins/module_utils/vmware.py
@@ -46,7 +46,7 @@ except ImportError:
 from ansible.module_utils._text import to_text, to_native
 from ansible.module_utils.six import integer_types, iteritems, string_types, raise_from
 from ansible.module_utils.basic import missing_required_lib
-from ansible.module_utils.six.moves.urllib.parse import unquote
+from urllib.parse import unquote
 from ansible_collections.community.vmware.plugins.module_utils._argument_spec import base_argument_spec
 
 
