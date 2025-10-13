e956f762a0a32ac9f22899ca7a710fa779f0b670
Mario Lenz <m@riolenz.de>
2025-10-13 16:28:57 +0200

    Replace iteritems

--- plugins/module_utils/vmware.py
+++ plugins/module_utils/vmware.py
@@ -45,7 +45,7 @@ except ImportError:
     HAS_PYVMOMI = False
 
 from ansible.module_utils._text import to_text, to_native
-from ansible.module_utils.six import iteritems, raise_from
+from ansible.module_utils.six import raise_from
 from ansible.module_utils.basic import missing_required_lib
 from urllib.parse import unquote
 from ansible_collections.vmware.vmware.plugins.module_utils.argument_spec import base_argument_spec
@@ -1838,7 +1838,7 @@ class PyVmomi(PyvmomiClient):
         Returns:
           dict, with u merged into d
         """
-        for k, v in iteritems(u):
+        for k, v in u.items():
             if isinstance(v, collections_compat.Mapping):
                 d[k] = self._deepmerge(d.get(k, {}), v)
             else:
